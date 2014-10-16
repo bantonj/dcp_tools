6f3edf3b19047d41b0cda2a7be55a6b08ddf3332
Joshua Banton <bantonj@gmail.com>
2014-10-15 21:09:29 -0400

    Fixed some bugs with decryption batch file creation. Still need to fix a bug where audio files are handled the same way as video files, need to make it to .wav, no rewrap, but ffmpeg remux to get the audio channel mapping metadata in there.

--- dcp_make_convert_bat_file.py
+++ dcp_make_convert_bat_file.py
@@ -6,7 +6,7 @@ import argparse
 
 ffmpeg_path = r"C:\software\ffmpeg-20131208-git-ae33007-win32-static\ffmpeg-20131208-git-ae33007-win32-static\bin\ffmpeg.exe"
 asdcp_path = r"C:\software\asdcp\asdcp-test.exe"
-open_dcp_cli = r"C:\open_dcp.exe"
+open_dcp_cli = r'"C:\Program Files\OpenDCP\bin\opendcp_mxf.exe"'
 
 def parse_xml(xml):
     try:
@@ -36,10 +36,16 @@ def parse_cpl_mxf(cpl, ASSETMAP):
     for reel in parsed.CompositionPlaylist.ReelList.Reel:
         try:
             mxf_list.append({"mainpicture_id": reel.AssetList.MainPicture.Id.value, "mainpicture_name": get_xml_path(reel.AssetList.MainPicture.Id.value, ASSETMAP),
-                            "mainsound_id": reel.AssetList.MainSound.Id.value, "mainsound_name": get_xml_path(reel.AssetList.MainSound.Id.value, ASSETMAP) })
+                            "mainpicture_key_id": reel.AssetList.MainPicture.KeyId.value,
+                            "mainsound_id": reel.AssetList.MainSound.Id.value, "mainsound_name": get_xml_path(reel.AssetList.MainSound.Id.value, ASSETMAP),
+                            "mainsound_key_id": reel.AssetList.MainSound.KeyId.value})
         except AttributeError:
             mxf_list.append({"mainpicture_id": parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainPicture.Id.value, 
-                            "mainpicture_name": get_xml_path(parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainPicture.Id.value, ASSETMAP)})
+                            "mainpicture_name": get_xml_path(parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainPicture.Id.value, ASSETMAP),
+                            "mainpicture_key_id": parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainPicture.KeyId.value,
+                            "mainsound_id": parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainSound.Id.value, 
+                            "mainsound_name": get_xml_path(parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainSound.Id.value, ASSETMAP),
+                            "mainsound_key_id": parsed.CompositionPlaylist.ReelList.Reel.AssetList.MainSound.KeyId.value, })
             break
     return mxf_list
     
@@ -80,16 +86,17 @@ def create_bash_file(pkl, assetmap, output_file, output_dir):
     with open(output_file, 'w') as f:
         f.write(bash)
         
-def create_decrypt_text(key_string, cpl_dict, output_dir):
-    media_key = ('mainpicture_id', 'mainpicture_name') if key_string.split(':')[1] == 'MDIK' else ('mainsound_id', 'mainsound_name')
+def create_decrypt_text(key_string, cpl_dict, output_dir, mxf_input_dir):
+    media_key = ('mainpicture_key_id', 'mainpicture_name') if key_string.split(':')[1] == 'MDIK' else ('mainsound_key_id', 'mainsound_name')
     for x in cpl_dict:
         if x[media_key[0]].split(':')[2] == key_string.split(':')[0]:
             jpeg_out_dir = os.path.join(output_dir, x[media_key[1]].split('.mxf')[0]) + r'/'
-            return "%s -k %s -x %s %s\n\n" % (asdcp_path, key_string.split(':')[2].strip(), jpeg_out_dir, x[media_key[1]]), jpeg_out_dir
+            mxf_input = os.path.join(mxf_input_dir, x[media_key[1]])
+            return "%s -k %s -x %s %s\n\n" % (asdcp_path, key_string.split(':')[2].strip(), jpeg_out_dir, mxf_input), jpeg_out_dir
       
 def create_wrap_text(jpeg_out_dir, output_dir):
     wrap_path = os.path.join(output_dir, jpeg_out_dir[:-1] +'_decrypted.mxf')
-    return "%s -i %s %s\n\n" % (open_dcp_cli, jpeg_out_dir, wrap_path), wrap_path
+    return "%s -i %s -o %s\n\n" % (open_dcp_cli, jpeg_out_dir[:-1], wrap_path), wrap_path
     
 def create_conv_text(decrypt_mxf, output_dir):
     video_out = os.path.join(output_dir, os.path.join(output_dir, decrypt_mxf.split('.mxf')[0] + '.mov'))
@@ -99,8 +106,9 @@ def create_decrypt_script(cpl, key_file, asssetmap, output_file, output_dir):
     f = open(key_file)
     cpl_dict = parse_cpl_mxf(cpl, asssetmap)
     script_text = ''
+    mxf_input_dir = os.path.dirname(cpl)
     for key in f.readlines():
-        decrypt_text, jpeg_out_dir = create_decrypt_text(key, cpl_dict, output_dir)
+        decrypt_text, jpeg_out_dir = create_decrypt_text(key, cpl_dict, output_dir, mxf_input_dir)
         script_text += decrypt_text
         wrap_text, decrypt_mxf = create_wrap_text(jpeg_out_dir, output_dir)
         script_text += wrap_text
